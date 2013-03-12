937fcee6332c8b435f6c1b5ef3dcb7d0df726d5a
awentz <austin.wentz@mines.sdsmt.edu>
2013-03-12 11:10:41 -0600

    more server code

---
+++ src/python/renard_flask.py
@@ -0,0 +1,26 @@
+
+
+from flask import *
+app = Flask(__name__)
+
+
+chan_vals = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+#routing
+
+@app.route('/')
+def index():
+	return 'Interactive Lighting'
+
+@app.route('/get_channel_vals')
+def get_channel_vals():
+	return jsonify(vals=chan_vals)
+
+@app.route('/set_channel_vals', methods=['POST'])
+def set_channel_vals():
+	chan_vals = json.loads(request.json)['vals']
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run('0.0.0.0')
+
